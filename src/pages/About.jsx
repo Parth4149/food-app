5349870ffebbca4e0b4d99c5019d2f50a0e4f6d4
Parth Prajapati <pp41495233@gmail.com>
2023-05-31 16:02:32 +0530

    Merge branch body

---
+++ src/pages/About.jsx
@@ -0,0 +1,14 @@
+import Header from "../components/Header";
+
+const About = () => {
+  return (
+    <>
+      <Header />
+      <div className="mt-24 align-center">
+        <h2>About us page</h2>
+      </div>
+    </>
+  );
+};
+
+export default About;
