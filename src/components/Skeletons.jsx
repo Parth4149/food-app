5260308f4fadf82b601bc9f0e081577cc4523e81
Parth Prajapati <pp41495233@gmail.com>
2023-06-05 18:22:48 +0530

    Remove duplicate code

--- src/components/Skeletons.jsx
+++ src/components/Skeletons.jsx
@@ -3,7 +3,7 @@ import SkeletonRestaurantCard from "./ui/SkeletonRestaurantCard";
 const Skeletons = () => {
   return (
     <section className="seeAllRestaurants">
-      {Array(8)
+      {Array(12)
         .fill("")
         .map((_, i) => (
           <article className="restaurant__card" key={i}>
--- src/pages/RestaurantInfo.jsx
+++ src/pages/RestaurantInfo.jsx
@@ -1,12 +1,15 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { IMG_CDN_URL } from "../../src/data";
+import TopPickSection from "../components/ui/TopPickSection";
+import RestaurantInfoSection from "../components/ui/RestaurantInfoSection";
 
 const RestaurantInfo = () => {
   const { restaurantId } = useParams();
   const [restaurantData, setRestaurantData] = useState(null);
-  const [recommendedRestaurantData, setRecommendedRestaurantData] =
+  const [restaurantInfoSectionData, setRestaurantInfoSectionData] =
     useState(null);
+  // const [recommendedRestaurantData, setRecommendedRestaurantData] = useState(null);
 
   useEffect(() => {
     getRestaurantInfo();
@@ -17,29 +20,23 @@ const RestaurantInfo = () => {
       `https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=22.5645175&lng=72.928871&restaurantId=${restaurantId}&submitAction=ENTER`
     );
     const json = await data.json();
-    // console.log(
-    //   json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+    // json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.forEach(
+    //   (card) => {
+    //     if (card?.card?.card?.title === "Recommended") {
+    //       console.log(card?.card?.card?.itemCards);
+    //       setRestaurantInfoSectionData(card?.card?.card?.itemCards);
+    //     }
+    //   }
     // );
-    json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards.forEach(
-      (card) => {
-        if (card?.card?.card?.title === "Recommended") {
-          console.log(card?.card?.card?.title, card?.card?.card?.itemCards);
-          setRecommendedRestaurantData(card?.card?.card?.itemCards);
-        }
-      }
+    // json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
+    setRestaurantInfoSectionData(
+      json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards
     );
-    if (
-      json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-        ?.card?.title === "Recommended"
-    ) {
-      setRecommendedRestaurantData(
-        json?.data?.cards[2]?.groupedCard?.cardGroupMap?.REGULAR?.cards[2]?.card
-          ?.card?.itemCards
-      );
-    }
     setRestaurantData(json?.data?.cards[0]?.card?.card?.info);
   }
-  console.log("recommended", recommendedRestaurantData);
+  // console.log("recommended", restaurantInfoSectionData);
+  console.log(document.querySelector(".restinfo-section details")?.open);
+
   return (
     <main className="restaurantInfo-main">
       {!restaurantData ? (
@@ -53,41 +50,29 @@ const RestaurantInfo = () => {
               src={IMG_CDN_URL + restaurantData?.cloudinaryImageId}
               alt={restaurantData.name}
             />
-            <h2>Name {restaurantData?.name}</h2>
+            <h2>{restaurantData?.name}</h2>
             <p>{restaurantData?.cuisines.join(", ")}</p>
             <p>{restaurantData?.areaName}</p>
             <div>⭐ {restaurantData?.avgRating}</div>
           </div>
         </section>
       )}
-
-      {!recommendedRestaurantData ? (
+      {!restaurantInfoSectionData ? (
         <section>
           <h2>Loading...</h2>
         </section>
       ) : (
-        <section className="recommended">
-          <h3 className="mt-24">Recommended</h3>
-          {recommendedRestaurantData.map((item) => (
-            <div className="recommended-inner" key={item?.card?.info?.id}>
-              {/* {console.log(item?.card?.info)} */}
-              <div>
-                <h4>{item?.card?.info?.name}</h4>
-                <h5>
-                  ₹{" "}
-                  {(!item?.card?.info?.defaultPrice
-                    ? item?.card?.info?.price
-                    : item?.card?.info?.defaultPrice) / 100}
-                </h5>
-              </div>
-              <figure>
-                <img
-                  src={IMG_CDN_URL + item?.card?.info?.imageId}
-                  alt={item?.card?.info?.name}
-                />
-              </figure>
-            </div>
-          ))}
+        <section className="restinfo-section-container">
+          {restaurantInfoSectionData.map((card) => {
+            if (card?.card?.card?.title === undefined) {
+              return;
+            }
+            return card?.card?.card?.title === "Top Picks" ? (
+              <TopPickSection />
+            ) : (
+              <RestaurantInfoSection card={card?.card?.card} />
+            );
+          })}
         </section>
       )}
     </main>
